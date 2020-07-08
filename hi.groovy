4dc9d0c4e42b2ba8f66cb47ab6f67a873ef60ef7
mayar-1995 <62892023+mayar-1995@users.noreply.github.com>
2020-07-08 15:53:58 +0200

    Update hi.groovy

--- hi.groovy
+++ hi.groovy
@@ -1,1 +1,12 @@
 print "Hello World!\n"
+String str = "India" 
+compareString(str) 
+
+def compareString(String str){ 
+  def str2 = "india" 
+  if( str2.toUpperCase() == str.toUpperCase() ) { 
+    println "same" 
+  }else{ 
+    println "not same" 
+  } 
+}
